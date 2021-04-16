bce26f645d7ef4a50508975e4b146da9f7f4fcce
jp8117 <jp8117@live.com>
2021-04-16 05:50:03 -0400

    Add project files.

---
+++ ClassesStuffs/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesStuffs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciate the variable declared at the bottom
+            players = new List<SoccerPlayer>();
+                
+            //call the add players method to add the players to the list
+            AddPlayers();
+
+            //loop through the players list "List<SoccerPlayer> players"
+            foreach (SoccerPlayer player in players)
+            {
+                //display every player who's JerseyNum is greater than 100
+                if (player.JerseyNum >= 100)
+                {
+                    //calls this player's DisplayInfo method and gets the data that corrisponds to them and only them.
+                    player.DisplayInfo();
+                }
+            }
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+
+        static void AddPlayers()
+        {
+            //dispite not passing the variable "players" to this method or declaring it locally, it can still be accessed
+            //and modified here
+            players.Add(new SoccerPlayer("Jim", 42, SoccerPlayer.Genders.FEMALE));
+            players.Add(new SoccerPlayer("Paul", 39, SoccerPlayer.Genders.MONKEY));
+            players.Add(new SoccerPlayer("IDFK", 420, SoccerPlayer.Genders.FEMALE));
+            players.Add(new SoccerPlayer("bubba", 41, SoccerPlayer.Genders.MONKEY));
+            players.Add(new SoccerPlayer("heinz", 57, SoccerPlayer.Genders.MALE));
+            players.Add(new SoccerPlayer("s'tan", 666, SoccerPlayer.Genders.MALE));
+            players.Add(new SoccerPlayer("Pete", 2, SoccerPlayer.Genders.MALE));
+        }
+
+        //this variable is now accessible by any method in the "Program" class
+        static List<SoccerPlayer> players;
+    }
+}
---
+++ ClassesStuffs/SoccerPlayer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesStuffs
+{
+    class SoccerPlayer
+    {
+        public enum Genders
+        {
+            MALE,
+            FEMALE,
+            MONKEY
+        }
+
+        /// <summary>
+        /// this is a special type of method called a constructor. Notice it doesn't have a return type.
+        /// </summary>
+        /// <param name="name">Name of the player</param>
+        /// <param name="jerseyNum">the player's jersey num</param>
+        /// <param name="gender">the player's gender (this will default to MONKEY [not my idea] if no gender is chosen)</param>
+        public SoccerPlayer(string name, int jerseyNum, Genders gender = Genders.MONKEY)
+        {
+            //sets the variable feilds of this instance of the class
+            Name = name;
+            JerseyNum = jerseyNum;
+            Gender = gender;
+        }
+
+        /// <summary>
+        /// Display player's info to the console
+        /// </summary>
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name} Jersey#: {JerseyNum}");
+        }
+
+        //declares a variable that can used from anywhere in the program but only set from within the class
+        public string Name
+        {
+            get; // tells the program you can access this variable from anywhere
+            private set; // tells the program you can ONLY set the variable from within this class
+        }
+        //declares a variable that can used from anywhere in the program but only set from within the class
+        public int JerseyNum
+        {
+            get; // tells the program you can access this variable from anywhere
+            private set; // tells the program you can ONLY set the variable from within this class
+        }
+        //declares a variable that can used from anywhere in the program but only set from within the class
+        public Genders Gender
+        {
+            get; // tells the program you can access this variable from anywhere
+            private set; // tells the program you can ONLY set the variable from within this class
+        }
+    }
+}
